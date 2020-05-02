d35a189e187c8423c59bf7cda57b4c550c4d7d95
Gospel Lekia <gospellekia@Gospels-MacBook-Pro.local>
2020-05-02 17:53:55 +0100

    print products

--- products.js
+++ products.js
@@ -1,5 +1,22 @@
 let products = ['Underpants:6.99', 'Socks:5.99', 'T-shirt:14.99', 'Trousers:31.99', 'Shoes:23.99'];
+const list = document.querySelector('ul');
+const totalBox = document.querySelector('p');
+totalBox.textContent = '';
+let total = 0;
+let name = [];
+let price = [];
+list.innerHTML = '';
+for (let i = 0; i < products.length; i++) {// number 2
+    let split = products[i].split(':');
+    let name = split[0];
+    let price = Number(split[1]);
+    total += price; 
 
-for (let i = 0; i < products.length; i++) {
-    // number 2
+     // number 5
+  let itemText = name + ' - $' + price;
+  
+  const listItem = document.createElement('li');
+  listItem.textContent = itemText;
+  list.appendChild(listItem);
 }
+totalBox.textContent = 'Total: $' + total.toFixed(2);
